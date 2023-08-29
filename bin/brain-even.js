2a4f15417d501dafd1483aae6c21301a0be2827d
ruslan <bumerboy86@gmail.com>
2023-08-29 16:11:48 +0600

    Fixed logic brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,27 +1,33 @@
 const askQuestion = require('../src/cli');
 
-const isEvenGame = (name) => {
-  const userName = name;
-  const isEven = (number) => number % 2 === 0;
-  let step = 0;
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  while (true) {
-    let sicretNumber = Math.floor(Math.random() * 100) + 1;
-    const correctAnswer = isEven(sicretNumber) ? 'yes' : 'no';
-    const question = askQuestion(`Question: ${sicretNumber} `);
-    if (correctAnswer === question) {
-      console.log('Correct!');
-      step += 1;
-      sicretNumber = Math.floor(Math.random() * 100) + 1;
-    } else {
-      return console.log(
-        `'${question}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`,
-      );
-    }
-    if (step === 3) {
-      return console.log(`Congratulations, ${userName}!`);
-    }
-  }
-};
+console.log('Welcome to the Brain Games!');
 
-module.exports = isEvenGame;
+const userName = askQuestion('May I have your name?: ');
+
+console.log(`Hello, ${userName}!`);
+
+const isEven = (number) => number % 2 === 0;
+
+const step = 3;
+let finish = false;
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let i = 0; i < step; i += 1) {
+  const sicretNumber = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = isEven(sicretNumber) ? 'yes' : 'no';
+  const question = askQuestion(`Question: ${sicretNumber} `);
+  if (correctAnswer === question) {
+    console.log('Correct!');
+    finish = true;
+  } else {
+    console.log(
+      `'${question}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`,
+    );
+    finish = false;
+    break;
+  }
+}
+if (finish) {
+  console.log(`Congratulations, ${userName}!`);
+}
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
 const askQuestion = require('../src/cli');
-const isEvenGame = require('./brain-even');
 
 console.log('Welcome to the Brain Games!');
 
 const userName = askQuestion('May I have your name?: ');
 
 console.log(`Hello, ${userName}!`);
-
-isEvenGame(userName);
