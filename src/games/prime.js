b4f2953b46de4d0f8b7366b5f47c5e7a2e7badf8
ruslan <bumerboy86@gmail.com>
2023-09-05 19:06:26 +0600

    Changed the function for determining prime numbers 2

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,9 +5,10 @@ import getRandNum from '../utils/getRandNum.js';
 const primeGame = () => {
   const isPrime = (num) => {
     if (num <= 1) return false;
-
-    for (let i = 2; i <= Math.sqrt(num); i += 1) {
-      if (num % i === 0) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
     }
     return true;
   };
