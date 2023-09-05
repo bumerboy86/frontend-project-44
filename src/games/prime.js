be698c073dec9665c150ed318586909c0c9930b9
ruslan <bumerboy86@gmail.com>
2023-09-05 19:39:54 +0600

    attempt to improve the function of calculating a prime number for https://codeclimate.com/ 1

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,10 +4,15 @@ import getRandNum from '../utils/getRandNum.js';
 
 const primeGame = () => {
   const isPrime = (num) => {
-    for (let i = 2; i < num; i += 1) {
-      if (num % i === 0) return false;
+    if (num <= 1) {
+      return false;
     }
-    return num !== 1;
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
   };
 
   const randomtNum = getRandNum(1, 100);
