38e67c46a530a642ef867e46f75fc5a66131d0c2
ruslan <bumerboy86@gmail.com>
2023-09-05 19:20:13 +0600

    Changed the function for determining prime numbers 3

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,14 +4,21 @@ import getRandNum from '../utils/getRandNum.js';
 
 const primeGame = () => {
   const isPrime = (num) => {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
-    if (num % 2 === 0 || num % 3 === 0) return false;
-    for (let i = 5; i * i <= num; i += 6) {
-      if (num % i === 0 || num % (i + 2) === 0) return false;
+    if (Number.isNaN(num) || !Number.isFinite(num) || num % 1 || num < 2) {
+      return false;
     }
+
+    const squareRoot = Math.sqrt(num);
+
+    for (let i = 2; i <= squareRoot; i += 1) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+
     return true;
   };
+
   const randomtNum = getRandNum(1, 100);
   const correctAnswer = isPrime(randomtNum) ? 'yes' : 'no';
   return getDataReturn(
